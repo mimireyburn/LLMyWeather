b5aa18565d1471eeeaa98dbac435eabbcc946ff7
Mimi Reyburn <79009541+mimireyburn@users.noreply.github.com>
2022-09-25 14:49:11 +0100

    add descriptions and api call

--- wthr.py
+++ wthr.py
@@ -1,57 +1,86 @@
 import json 
+import requests
+import os
+
+KEY = os.environ['MET_OFFICE_API']
+# print(os.environ)
+
+descr = {
+    0 : "not",
+    1 : "a tiny bit",
+    2 : "slightly",
+    3 : "fairly",
+    4 : "a bit",
+    5 : "quite",
+    6 : "very",
+    7 : "o shit it's",
+    8 : "extremely",
+    9 : "disgustingly",
+    10: "FUCKIN"
+}
+
+# with open('test.json') as fp:
+#     data = json.load(fp)
 
 
-with open('test.json') as fp:
-    data = json.load(fp)
+# westminster = 354160
+# orkney = 353745
 
+data = requests.get("http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/353745?res=3hourly&key=" + KEY).text
+data = json.loads(data)
 
 today = data["SiteRep"]["DV"]["Location"]["Period"][0]["Rep"]
+today.pop(0)
 
-
-mydict = {"Rain":{"ID": "Pp", "impact_max_positive": 100, "impact_zero": 0, "weight":1},
-    "Wind":{"ID": "S", "impact_max_positive":50, "impact_zero": 0, "weight": 1},
-    "Temp":{"ID": "F", "impact_max_positive":40, "impact_zero":16, "impact_max_negative":-5, "weight":1}}
+mydict = {"rainy":{"ID": "Pp", "maximum_impact": 100, "zero_impact": 0, "weight":1},
+    "windy":{"ID": "S", "maximum_impact":30, "zero_impact": 0, "weight": 0.6},
+    "hot":{"ID": "F", "maximum_impact":40, "zero_impact":20, "weight":0.7},
+    "cold":{"ID": "F", "maximum_impact":-5, "zero_impact":20, "weight":0.7}}
 
 periods = {"0": "at night", "180": "early morning", "360": "in the morning", "540": "late morning", "720": "early afternoon", "900": "late afternoon", "1080": "in the evening", "1260": "late evening"}
 
-# mydict = {"Temp":{"ID": "F", "impact_max_positive":40, "impact_zero":16, "impact_max_negative":-5, "weight":1}}
 
 output = {}
 
 for window in today:
 
-    print(periods[window["$"]])
+    # print(periods[window["$"]])
 
     for weather_type in mydict.keys():
         id = mydict[weather_type]["ID"]
         val = int(window[id])
+            
+        zero_impact = mydict[weather_type]["zero_impact"]
+        maximum_impact = mydict[weather_type]["maximum_impact"]
 
-        impact_zero = mydict[weather_type]["impact_zero"]
-
-        impact_max_positive = mydict[weather_type]["impact_max_positive"]
-        val = min(val, impact_max_positive)
-
-        try:
-            impact_max_negative = mydict[weather_type]["impact_max_negative"]
-            val = max(val, impact_max_negative)
-        except KeyError:
-            pass
-
-        if val >= impact_zero:
-            norm = round((val - impact_zero)/(impact_max_positive - impact_zero), 2) 
+        if maximum_impact < zero_impact:
+            val = min(max(val, maximum_impact), zero_impact)
         else:
-            norm = round((impact_zero - val)/(impact_zero - impact_max_negative), 2)
+            val = max(min(val, maximum_impact), zero_impact)
 
-        print("Weather type: ", weather_type, "Value: ", val, "Importance: ", norm)
-        output[norm] = weather_type + " " + str(val) + " " + periods[window["$"]]
+        norm = (val - zero_impact)/(maximum_impact - zero_impact) * 10
+        weight = mydict[weather_type]["weight"]
 
-    print("______")
+        
+        norm = abs(round(norm))
+        # print("Weather type: ", weather_type, "Value: ", val, "Importance: ", norm)
+        # print(descr[norm])
 
-sorted_output = list(reversed(sorted(output.items())))
+        statement = str(descr[norm] + " " + weather_type + " " + periods[window["$"]])
+
+        output[statement] = norm * weight
+
+    # print("______")
+
+# print(output)
+sorted_output = sorted(output.items(), key=lambda x: x[1], reverse=True)
+# print(sorted_output)
+
+# sorted_output = list(reversed(sorted(output.items())))
 # print(sorted_output[0:3])
 
 for item in sorted_output[0:3]: 
-    print(item[1])
+    print(item[0])
 
     # precip = window[params["Precip"]["ID"]]
     # wind = window[params["Wind"]["ID"]]
@@ -59,3 +88,4 @@ for item in sorted_output[0:3]:
 
     # print(precip, temp, wind)
 
+  
