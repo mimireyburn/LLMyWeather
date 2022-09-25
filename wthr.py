c9e4b289fac5a77557b3efed1bbeab95e928932d
Mimi Reyburn <79009541+mimireyburn@users.noreply.github.com>
2022-09-25 12:41:11 +0100

    add times of day

--- wthr.py
+++ wthr.py
@@ -8,13 +8,15 @@ with open('test.json') as fp:
 today = data["SiteRep"]["DV"]["Location"]["Period"][0]["Rep"]
 
 
-mydict = {"Precip":{"ID": "Pp", "impact_max_positive": 100, "impact_zero": 0, "weight":1},
+mydict = {"Rain":{"ID": "Pp", "impact_max_positive": 100, "impact_zero": 0, "weight":1},
     "Wind":{"ID": "S", "impact_max_positive":50, "impact_zero": 0, "weight": 1},
     "Temp":{"ID": "F", "impact_max_positive":40, "impact_zero":16, "impact_max_negative":-5, "weight":1}}
 
-periods = {"0": "night", "180": "early morning", "360": "morning", "540": "late morning", "720": "early afternoon", "900": "late afternoon", "1080": "evening", "1260": "late evening"}
+periods = {"0": "at night", "180": "early morning", "360": "in the morning", "540": "late morning", "720": "early afternoon", "900": "late afternoon", "1080": "in the evening", "1260": "late evening"}
 
 # mydict = {"Temp":{"ID": "F", "impact_max_positive":40, "impact_zero":16, "impact_max_negative":-5, "weight":1}}
+
+output = {}
 
 for window in today:
 
@@ -41,7 +43,15 @@ for window in today:
             norm = round((impact_zero - val)/(impact_zero - impact_max_negative), 2)
 
         print("Weather type: ", weather_type, "Value: ", val, "Importance: ", norm)
+        output[norm] = weather_type + " " + str(val) + " " + periods[window["$"]]
+
     print("______")
+
+sorted_output = list(reversed(sorted(output.items())))
+# print(sorted_output[0:3])
+
+for item in sorted_output[0:3]: 
+    print(item[1])
 
     # precip = window[params["Precip"]["ID"]]
     # wind = window[params["Wind"]["ID"]]
