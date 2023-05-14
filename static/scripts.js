343b86b434b5a4c7efb4022740c20f0ddd6128cf
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2023-05-14 16:11:24 +0200

    Support haikus

--- static/scripts.js
+++ static/scripts.js
@@ -6,11 +6,27 @@ function adjustFontSize() {
   const element = document.getElementById("overlay-text");
   const text = element.innerHTML;
 
-  const length = text.length;
-  let fontSize = 16; // Default font size in pixels.
+  let fontSize = 0;
 
-  if (length > 0) {
-    fontSize = 2600 / length;
+  if (text.includes("<br>")) {
+    const sentences = text.split("<br>");
+    let maxSentenceLength = 0;
+
+    sentences.forEach((sentence) => {
+      if (sentence.length > maxSentenceLength) {
+        maxSentenceLength = sentence.length;
+      }
+    });
+
+    if (maxSentenceLength > 0) {
+      fontSize = 600 / maxSentenceLength; // coefficient for sentence length
+    }
+  } else {
+    const length = text.length;
+
+    if (length > 0) {
+      fontSize = 2600 / length; // coefficient for total text length
+    }
   }
 
   element.style.fontSize = `${fontSize}px`;
